8fde2e83bd7fc348671cd2517b3cb4aa7852a38c
Priyanshu Bisht <priyanshubisht451@gmail.com>
2024-10-23 14:41:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl underline">
+        Safe-Nest WEB!
+      </h1>
+    </div>
+  );
+}
